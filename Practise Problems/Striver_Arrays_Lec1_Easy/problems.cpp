ca8ed125d0347e680dcc6433075b764505e99b54
swaggyhustler <gedamsubhash086@gmail.com>
2024-07-25 15:01:13 +0530

    Practise code for Easy Level Array Questions

---
+++ Practise Problems/Striver_Arrays_Lec1_Easy/problems.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+void noAppearsOnes();
+void maxCountOfOnes();
+void longestConsecutiveSumEqualToKApproach1( int );
+void longestConsecutiveSumEqualToKApproach2( int );
+int main(){
+    longestConsecutiveSumEqualToKApproach2(6);
+    return 0;
+}
+void longestConsecutiveSumEqualToKApproach2(int k){
+    vector<int> arr={1,2,3,1,1,1,1,4,2,3};
+    map<int, int> preSumMap;
+    int max_len=0, sum=0;
+    for(int i=0; i<arr.size(); i++){
+        sum+=arr[i];
+        if(sum==k){
+            max_len=max(max_len, i+1);
+        }
+        int rem=sum-k;
+        if(preSumMap.find(rem)!=preSumMap.end()){
+            max_len=max(max_len, i-preSumMap[rem]);
+        }
+        preSumMap[sum]=i;
+    }
+    cout<<max_len<<endl;
+}
+void longestConsecutiveSumEqualToKApproach1(int k){
+    vector<int> arr={1,2,3,1,1,1,1,4,2,3};
+    int sum=0, max_len=0;
+    for(int i=0; i<arr.size(); i++){
+        sum=0;
+        for(int j=i; j<arr.size(); j++){
+            sum+=arr[j];
+            if(sum==k){
+                max_len=max(max_len, j-i+1);
+                cout<<sum<<" , "<<max_len<<endl;
+            }
+        }
+    }
+    cout<<max_len<<endl;
+}
+void noAppearsOnes(){
+    vector<int> arr={3,3,5,7,4,7,5,8,6,8,6};
+    int xor1=0;
+    for(int i=0; i<arr.size(); i++){
+        xor1^=arr[i];
+    }
+    cout<<xor1<<endl;
+}
+void maxCountOfOnes(){
+    vector<int> arr={1,0,1,1,0,1};
+    int left=0, maxi=0;
+    while(left<arr.size()){
+        int count=0;
+        while(left<arr.size() && arr[left]==0){
+            left++;
+        }
+        while(left<arr.size() && arr[left]==1){
+            count++;
+            left++;
+        }
+        maxi=max(count, maxi);
+    }
+    cout<<maxi<<endl;
+}
