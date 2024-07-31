b846b87433b8740f5bd36dc4df08f5b8808fc8e6
Gedam Subhash <131755190+swaggyhustler@users.noreply.github.com>
2024-07-31 18:56:11 +0530

    Time: 2 ms (53.67%) | Memory: 7.5 MB (7.07%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -4,10 +4,11 @@ public:
         if(time<n){
             return time+1;
         }else{
-            if(((time/(n-1))%2)==0){
-                return (time%(n-1))+1;
+            int rem=time%(n-1);
+            if(((time/(n-1))%2)){
+                return n-rem;
             }else{
-                return n-(time%(n-1));
+                return rem+1;
             }
         }
     }
